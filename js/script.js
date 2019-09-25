7c811b0d544a43e91b4ee0331d12b0e9208a1d41
preskot <preskot53@gmail.com>
2019-09-25 18:11:35 +0300

    header improved
    mobile hamburger done , ipad and desktop improved

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+    $(document).ready(function () {
+        $('.mobile-open').click(function () {
+            $(this).find('object').toggle();
+            $('.bottom-half-header').toggle();
+        });
+    });
+
