66c4cde75b08ef79cad3208f87066cb1bc5ec05b
preskot <preskot53@gmail.com>
2019-09-29 15:08:24 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 
 
-    // $(document).ready(function () {
-    //     // $('.mobile-open').click(function () {
-    //     //     $(this).find('object').toggle();
-    //     //     $('.bottom-half-header').toggle();
-    //     // });
+    $(document).ready(function () {
+        $('.mobile-open').click(function () {
+            $(this).find('object').toggle();
+            $('.bottom-half-header').toggle();
+        });
        
-    // });
+    });
 
 $(document).ready(function(){
-    $(function() {
-      $('.mobile-open').on('click', function(e) {
-        e.preventDefault();
-        $(this).find('object').toggle();
-        $('.bottom-half-header').toggle();
-      });
-
-      $(window).resize(function(){
-        var width = $(this).width();
-        if(width > 480 && $('.bottom-half-header').is(':hidden')) {
-            $('.bottom-half-header').removeAttr('style');
-        }
-      });
-    });
+    // $(function() {
+    //   $('.mobile-open').on('click', function(e) {
+    //     e.preventDefault();
+    //     $(this).find('object').toggle();
+    //     $('.bottom-half-header').toggle();
+    //   });
+    //
+    //   $(window).resize(function(){
+    //     var width = $(this).width();
+    //     if(width > 480 && $('.bottom-half-header').is(':hidden')) {
+    //         $('.bottom-half-header').removeAttr('style');
+    //     }
+    //   });
+    // });
 
    $(".collapse.show").each(function(){
    $(this).prev(".card-header").find(".unactive").addClass("accordion__up-icon").removeClass("accordion__down-icon");
