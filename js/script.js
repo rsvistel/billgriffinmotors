4c1a2bcb5c50b258193bd8a6aa0dac34727586d8
romkravets <romann.kravets@gmail.com>
2019-09-30 17:53:50 +0300

    mobile-accordion

--- js/script.js
+++ js/script.js
@@ -24,64 +24,17 @@ $(document).ready(function(){
 
     $(".more").click(function() {
         var elem = $(".more").text();
-        if (elem == "more...") {
-            $(".more").text("less...");
+        if (elem == "more") {
+            $(".more").text("less");
             $(".complete").show();
-            $(".fa").addClass('fa-angle-up').removeClass('fa-angle-down');
+            $(".more-icon").addClass('fa-angle-up').removeClass('fa-angle-down');
 
         } else {
-            $(".more").text("more...");
+            $(".more").text("more");
             $(".complete").hide();
-            $(".fa").addClass('fa-angle-down').removeClass('fa-angle-up');
+            $(".more-icon").addClass('fa-angle-down').removeClass('fa-angle-up');
         }
     });
-    
-    // $('#carouselUsedCar').bind('slid', function() {
-    //     currentIndex = $('div.active').index() + 1;
-    //    $('.num').html(''+currentIndex+'/'+totalItems+'');
-    //    console.log( $('.num').html(''+currentIndex+'/'+totalItems+''))
-    // });
-    // $('#carouselUsedCar').on('slid.bs.carousel', function() {
-    //     currentIndex = $('div.active').index() + 1;
-    //    $('.num').html(''+currentIndex+'/'+totalItems+'');
-    // });
-    // var totalItems = $('.carousel-item').length;
-    //     var currentIndex = $('.carousel-item.active').index() + 1;
-
-    //     var down_index;
-    //     $('.num').html(''+currentIndex+'/'+totalItems+'');
-
-    //         $(".carousel-control-next").click(function(){
-    //         currentIndex_active = $('.carousel-item.active').index() + 2;
-    //         if (totalItems >= currentIndex_active)
-    //         {
-    //             down_index= $('.carousel-item.active').index() + 2;
-    //             $('.num').html(''+currentIndex_active+'/'+totalItems+'');
-    //         }
-    //         });
-    // var totalItems = $('.carousel-item').length;
-    //         var currentIndex = $('div.carousel-item.active').index() + 1;
-
-    //         var down_index;
-    //         $('.num').html(''+currentIndex+'/'+totalItems+'');
-
-    //             $(".carousel-control-next").click(function(){
-    //             currentIndex_active = $('div.carousel-item.active').index() + 2;
-    //             if (totalItems >= currentIndex_active)
-    //             {
-    //                 down_index= $('div.carousel-item.active').index() + 2;
-    //                 $('.num').html(''+currentIndex_active+'/'+totalItems+'');
-    //             }
-    //         });
-
-    //             $(".carousel-control-prev").click(function(){
-    //                 down_index=down_index-1;
-    //             if (down_index >= 1 )
-    //             {
-    //                 $('.num').html(''+down_index+'/'+totalItems+'');
-    //             }
-    //         });
-
 });
 
 
